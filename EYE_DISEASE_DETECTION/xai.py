4d9dc83bcf4014d41d0ebd2df13cb07fc9684912
SOORAJRAJUTV <89466147+SOORAJRAJUTV@users.noreply.github.com>
2024-10-13 19:48:27 +0530

    Add files via upload

---
+++ EYE_DISEASE_DETECTION/xai.py
@@ -0,0 +1,77 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import cv2
+
+from lime import lime_image
+from skimage.segmentation import mark_boundaries
+from skimage.color import gray2rgb
+
+
+# ResNet50+ - TensorFlow Model Loading
+
+model = tf.keras.models.load_model('model_resnet50.h5')
+explainer = lime_image.LimeImageExplainer()
+
+
+def generate_lime_explanation(model, image):
+    explanation = explainer.explain_instance(image[0].astype('double'), model.predict, top_labels=1, hide_color=0, num_samples=1000)
+    lime_explanation, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=True, num_features=5, hide_rest=False)
+
+    # Convert Lime explanation to RGB format
+    lime_explanation_rgb = gray2rgb(lime_explanation)
+
+    # Overlay Lime explanation on the original image using the mask
+    marked_explanation = mark_boundaries(image[0], mask)
+
+    return marked_explanation
+
+# Function to preprocess image
+def preprocess_image(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
+    img = cv2.resize(img, (256, 256))  # Resize to match model input shape
+    img = np.expand_dims(img, axis=0)  # Add batch dimension
+    img = img.astype('float32') / 255  # Normalize
+    return img
+
+# Provide the path to your image
+image_path = r"C:\Users\soora\OneDrive\Desktop\WEBpro\static\4637_right.jpg"
+
+# Read and preprocess the image
+img = cv2.imread(image_path)
+if img is None:
+    print("Error: Unable to read the image.")
+else:
+    img = preprocess_image(img)
+
+    # Make predictions
+    class_labels = ["cataract", "diabetic_retinopathy", "glaucoma", "normal"]
+    class_index = np.argmax(model.predict(img))
+    predicted_class = class_labels[class_index]
+
+    print("Predicted class:", predicted_class)
+
+lime_explanation = generate_lime_explanation(model, img)
+
+# Plot results
+# fig, ax = plt.subplots(1, 2, figsize=(15, 5))
+# ax[0].imshow(img[0])
+# ax[0].set_title(f'Original Image')
+
+# ax[1].imshow(lime_explanation)
+# ax[1].set_title(f'LIME Explanation : {predicted_class}')
+
+# plt.show()
+
+
+fig, ax = plt.subplots(1, 2, figsize=(15, 5))
+
+ax[0].imshow(img[0])
+ax[0].set_title(f'Original Image')
+
+ax[1].imshow(lime_explanation)
+ax[1].set_title(f'LIME Explanation : {predicted_class}')
+
+# Save the figure
+plt.savefig('lime_explanation.png')  # Change the filename as needed
+plt.close()  # Close the figure to release memory
